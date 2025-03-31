66d415c0158b5f8f65b8077e47e7dd9c4b29da24
adityasays <aditya.shukla019r@gmail.com>
2025-04-01 03:54:19 +0530

    bugs fixes

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Provider from "@/provider";
 import TitleUpdater from "@/components/TitleUpdater"; 
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <TitleUpdater /> 
+          <TitleUpdater /> <SpeedInsights/> <Analytics/>
           <Provider>{children}</Provider>
         </body>
       </html>
